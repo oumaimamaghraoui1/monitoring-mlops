6dfd990bead091839c38932187bfefbccc55a191
oumaima MAGHRAOUI <oumaima.m.maghraoui@pwc.com>
2026-03-26 16:13:04 +0000

    fix(ci): make RCA inference model path environment-independent

--- monitoring-backend/mlops/inference/rca_predict.py
+++ monitoring-backend/mlops/inference/rca_predict.py
@@ -1,10 +1,18 @@
+from pathlib import Path
 import sys
 import json
 import joblib
 
 # Load trained model + encoder
-model = joblib.load("mlops/models/rca_model.pkl")
-encoder = joblib.load("mlops/models/rca_label_encoder.pkl")
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+MODEL_PATH = BASE_DIR / "mlops/models/rca_model.pkl"
+ENCODER_PATH = BASE_DIR / "mlops/models/rca_label_encoder.pkl"
+
+model = joblib.load(MODEL_PATH)
+encoder = joblib.load(ENCODER_PATH)
+
 
 # Read input from Node
 input_data = json.loads(sys.argv[1])
